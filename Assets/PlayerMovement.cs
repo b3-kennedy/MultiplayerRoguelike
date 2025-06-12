a7df54052cf5aa7b8054d99f531ab55ad71cc230
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-06-12 11:34:08 +0100

    removed slow down on landing

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -167,7 +167,7 @@ public class PlayerMovement : NetworkBehaviour
 
         if (wasAirborne && grounded)
         {
-            OnLand();
+            //OnLand();
             wasAirborne = false;
         }
 
